28154950ba6ca2e33b75caaa0dadde0023617bc9
Facundo Sanchez Galindo <facundo.sanchez.galindo@gmail.com>
2022-08-08 11:50:37 +0200

    Create UserFactory

--- Sat.Recruitment.Api/Controllers/UsersController.cs
+++ Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -39,53 +39,7 @@ namespace Sat.Recruitment.Api.Controllers
                     Errors = errors
                 };
 
-            var newUser = new User
-            {
-                Name = name,
-                Email = email,
-                Address = address,
-                Phone = phone,
-                UserType = userType,
-                Money = decimal.Parse(money)
-            };
-
-            if (newUser.UserType == "Normal")
-            {
-                if (decimal.Parse(money) > 100)
-                {
-                    var percentage = Convert.ToDecimal(0.12);
-                    //If new user is normal and has more than USD100
-                    var gif = decimal.Parse(money) * percentage;
-                    newUser.Money = newUser.Money + gif;
-                }
-                if (decimal.Parse(money) < 100)
-                {
-                    if (decimal.Parse(money) > 10)
-                    {
-                        var percentage = Convert.ToDecimal(0.8);
-                        var gif = decimal.Parse(money) * percentage;
-                        newUser.Money = newUser.Money + gif;
-                    }
-                }
-            }
-            if (newUser.UserType == "SuperUser")
-            {
-                if (decimal.Parse(money) > 100)
-                {
-                    var percentage = Convert.ToDecimal(0.20);
-                    var gif = decimal.Parse(money) * percentage;
-                    newUser.Money = newUser.Money + gif;
-                }
-            }
-            if (newUser.UserType == "Premium")
-            {
-                if (decimal.Parse(money) > 100)
-                {
-                    var gif = decimal.Parse(money) * 2;
-                    newUser.Money = newUser.Money + gif;
-                }
-            }
-
+            User newUser = UserFactory.Create(name, email, address, phone, userType, money);
 
             var reader = ReadUsersFromFile();
 
